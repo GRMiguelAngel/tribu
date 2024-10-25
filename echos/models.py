244fb1f35dca0598e6d0bae03a6d215cb5aa5661
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-10-25 17:52:26 +0100

    se suben correciciones

--- echos/models.py
+++ echos/models.py
@@ -3,12 +3,10 @@ from django.db import models
 
 # Create your models here.
 
+
 class Echo(models.Model):
     content = models.TextField(max_length=5000)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
-    user = models.ForeignKey(
-            settings.AUTH_USER_MODEL,
-            on_delete=models.CASCADE
-        )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
