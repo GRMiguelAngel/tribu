c4809cb2b70051fd1d5506be5fa3fcdc1e1fc4fa
GRMiguelAngel <miguelangel2e2005@gmail.com>
2024-11-14 13:13:45 +0000

    Se sube contenido de tribu

--- main/urls.py
+++ main/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from django.contrib.auth.views import LoginView
 import accounts.views
 
 urlpatterns = [
